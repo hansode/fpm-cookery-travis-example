0cef47ad311bc15fdd92a62b6177bc17230fa4d0
Masahito Yoshida <masahito@axsh.net>
2013-06-28 18:14:19 +0900

    replace fpm-cookery with redis.

--- .fpm-cookery.mk
+++ .fpm-cookery.mk
@@ -4,4 +4,5 @@ before_install:
 	git clone https://github.com/bernd/fpm-cookery.git
 
 script:
-	bundle exec fpm-cook fpm-cookery/recipes/redis/recipe.rb
+	#bundle exec fpm-cook fpm-cookery/recipes/redis/recipe.rb
+	bundle exec fpm-cook fpm-cookery/recipes/fpm-cookery/recipe.rb
